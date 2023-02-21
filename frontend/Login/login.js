8e0f722673dc87126867ffdd6fe8c55b0a820020
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-02-21 18:01:28 +0530

    Creating_Login_page

---
+++ frontend/Login/login.js
@@ -0,0 +1,25 @@
+async function signIn(e){
+    try{
+     e.preventDefault();
+  
+     let loginDetails = {
+        email:e.target.email.value,
+        password:e.target.password.value,
+     };
+  
+     if(loginDetails.email.length < 0){
+      alert('Enter a valid email');
+      return;
+     }else if(loginDetails.password.length < 8){
+      alert('Enter A strong password')
+     };
+  
+  let response = await axios.post('http://localhost:3000/user/signIn', loginDetails);
+          alert(response.data.message);
+         
+    }
+    catch(err){
+      document.body.innerHTML += `<div style="color:black">${err.message}<div>`;
+    }
+  };
+  
--- frontend/Signup/signup.js
+++ frontend/Signup/signup.js
@@ -1,40 +1,39 @@
 async function signup(e){
-try{
-      e.preventDefault();
+  try{
+        e.preventDefault();
+    
+        let signupDetails ={
+            name:e.target.name.value,
+            email:e.target.email.value,
+            phone:e.target.phone.value,
+            password:e.target.password.value
+        };
+      if(signupDetails.name.length < 6 || signupDetails.name === ''){
+        alert('Enter a valid name');
+        return ;
+      }else if(signupDetails.email.indexOf('@')==-1){
+        alert('Enter a valid email');
+        return ;
+      }else if(signupDetails.phone.length < 10 || signupDetails.password.length < 8){
+        alert('Enter a valid phone number or password ');
+        return ;
+      }
   
-      let signupDetails ={
-          name:e.target.name.value,
-          email:e.target.email.value,
-          phone:e.target.phone.value,
-          password:e.target.password.value
-      };
-    if(signupDetails.name.length < 6 || signupDetails.name === ''){
-      alert('Enter a valid name');
-      return ;
-    }else if(signupDetails.email.indexOf('@')==-1){
-      alert('Enter a valid email');
-      return ;
-    }else if(signupDetails.phone.length < 10 || signupDetails.password.length < 8){
-      alert('Enter a valid phone number or password ');
-      return ;
+        
+     await axios.post('http://localhost:3000/user/signup', signupDetails)
+     .then(response =>{
+         if(response.status ===201 ){
+      window.location.href='../Login/login.html';
+       }else{
+        alert("Sign Up Successful!")
     }
-
-      
-   await axios.post('http://localhost:3000/user/signup', signupDetails)
-   .then(response =>{
-       if(response.status ===201 ){
-        alert("You already have an account with us! Please Login...")
-     }else{
-      alert("Sign Up Successful!")
+     })
+     .catch(err=>{
+      throw new Error(err)
+     })
+    }
+  catch(err){
+    alert("You already have an account with us! Please Login...")
   }
-   })
-   .catch(err=>{
-    throw new Error(err)
-   })
-  }
-catch(err){
-  alert("You already have an account with us! Please Login...")
-}
-  }
-
-
+    }
+  
